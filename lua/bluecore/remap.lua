fc125da8365fa7b8e99ec135b9df0ee677fd1c64
BlueKossa <dexter@stormdal.se>
2023-11-03 03:41:20 +0100

    Added system yanking

--- lua/bluecore/remap.lua
+++ lua/bluecore/remap.lua
@@ -30,10 +30,7 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
-vim.keymap.set("v", "<leader>y", function()
-    vim.cmd.yank()
-    vim.cmd.Wsly()
-end)
+vim.keymap.set("v", "<leader>y", '"+y') 
 
 
 vim.keymap.set("n", "<leader><tab>", "<cmd>wincmd w<CR>")
