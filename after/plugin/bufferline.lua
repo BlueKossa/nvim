ce6621973faa69b5b9c4696e2d3fb0c4d6e3ac15
BlueKossa <dexter@stormdal.se>
2023-11-04 18:04:24 +0100

    Keybinds

--- after/plugin/bufferline.lua
+++ after/plugin/bufferline.lua
@@ -21,7 +21,7 @@ bufferline.setup {
 -- Keybinds
 vim.keymap.set("n", "<leader>tn", "<cmd>BufferLineCycleNext<CR>")
 vim.keymap.set("n", "<leader>tb", "<cmd>BufferLineCyclePrev<CR>")
-vim.keymap.set("n", "<leader>tc", "<cmd>BufferLineCloseLeft<CR>")
+vim.keymap.set("n", "<leader>tc", "<cmd>BufferLineClose<CR>")
 vim.keymap.set("n", "<leader>tt", function()
     local tab = vim.fn.getchar() - 48 -- 48 = ASCII 0
     vim.cmd("BufferLineGoToBuffer " .. tab)
